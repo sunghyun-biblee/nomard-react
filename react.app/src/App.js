3fdb84ee514c3d8356c66d70cd7786eaa69dd724
sunghyun1014 <sunghyun1014@naver.com>
2023-12-12 22:59:27 +0900

    map에 대해 복습하고 , 라우터를 만들어 실행과정이 어떻게 되는지 확인하여봄 / path 경로에 따라 지정한 element > component를 확인할 수 있음

--- react.app/src/App.js
+++ react.app/src/App.js
@@ -1,68 +1,28 @@
 import { useState, useEffect } from "react";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// router를 사용한 이후부터는 App.js는 router를 render 한다
+//router는 url을 보고있는 component이다.
+// url에 따라 home을 보여주거나 detail을 보여줄것이다.
 
 function App() {
-  const buy = (event) => {
-    event.preventDefault();
-    setSelectbuy(true);
-    console.log(money + "원");
-  };
-  const selectMoney = (e) => {
-    if (e.target.value.length > e.target.maxLength)
-      e.target.value = e.target.value.slice(0, e.target.maxLength);
-    setSelectbuy(false);
-    setMoney(e.target.value);
-  };
-  const selectCoin = (e) => {
-    setSelectbuy(false);
-    setGetcoin(e.target.value);
-    console.log(e.target.value);
-  };
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [money, setMoney] = useState();
-  const [selectbuy, setSelectbuy] = useState(false);
-  const [getcoin, setGetcoin] = useState();
-  useEffect(() => {
-    // fetch로 request 보내고 response로 데이터를 받아옴 > 받아온 response를 json 형식으로 변환 > json을 console에 출력
-    fetch("https://api.coinpaprika.com/v1/tickers?limit=10")
-      .then((Response) => Response.json())
-      .then((json) => {
-        setCoins(json); // 가져온 데이터를 setcoins에 할당하여 , coins를 변경
-        setLoading(false); // 첫 render에만 보여주기위해 값을 가져온이후 false로 변경하여  render 되지않게 함
-      });
-  }, []);
   return (
-    <div>
-      {/* coins의 usestate에 기본값을 지정해주지않으면 undefind가 나오므로 
-        아무값이 없는 기본값이라도 지정해주어야 h1에서 coins.length를 가져올때 오류가생기지않음
-        */}
-      <h1>Coins {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? (
-        <b>Loading..</b>
-      ) : (
-        <form onSubmit={buy}>
-          <select onChange={selectCoin}>
-            <option>select coin</option>
-            {coins.map((items, index) => (
-              <option key={index} value={items.quotes.USD.price}>
-                {items.name.toUpperCase()} [{items.first_data_at}] price:$
-                {items.quotes.USD.price.toFixed(2)}
-              </option>
-            ))}
-          </select>
-          <input
-            type="number"
-            placeholder="금액을 입력해주세요"
-            onChange={selectMoney}
-            maxLength={9}
-          />
-          <p>{money} 원 있습니다</p>
-          <button>구매하기</button>
-          {selectbuy ? <a>구매수량 {(money / getcoin).toFixed(100)}</a> : null}
-        </form>
-      )}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* path="/" : / 부분은 루트페이지로 home 페이지로 이동한다는 뜻 */}
+        <Route path="/movie" element={<Detail />} />
+        {/* path="/movie" 이 경로로 들어오면 detail 페이지로 이동 */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+{
+  /* map을 사용하여 새로운 array로 변형 , items와 index를 사용 (index의 사용도는 key를 넣어주기 위해 
+  react.js는 map을 사용할때 항상 key를 넣어주어야한다, key와 value가 한쌍이기때문에)
+React Router는 페이지를 전환해주는 것 : npm install react-router-dom */
+}
