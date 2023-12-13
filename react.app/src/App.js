46b756613216f4d1f7343e70e3d71131614686a3
sunghyun1014 <sunghyun1014@naver.com>
2023-12-13 23:04:25 +0900

    useparams에 대해 알아봄 전달받은 파라미터의 값을 활용한 페이지로 이동하고싶을때는 :(콜론)을 붙여 변수로 지정한후에 전달받은 파라미터값을 할당시켜주어야 한다. :(콜론)이 없는상태에서는 일반 텍스트경로로 인식하여 원하는 변수페이지로 이동할 수 없다.

--- react.app/src/App.js
+++ react.app/src/App.js
@@ -12,7 +12,17 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         {/* path="/" : / 부분은 루트페이지로 home 페이지로 이동한다는 뜻 */}
-        <Route path="/movie" element={<Detail />} />
+        <Route path="/movie/:movieid" element={<Detail />} />
+        {/* : (콜론)을 통해 뒤에오는 페이지를 동적으로 구현가능하다.
+        movie.js의 Link component에서 전달받은 파라미터값을 :(콜론)movieid 변수안에 저장
+        그리고 detail component에서 useparams을 사용하여 전달받은 파라미터 값을 활용
+
+        path경로에 변수의 경로를 작성하고싶다면 / : < 기호를 작성해주어야 한다. 
+        ex)path="/movie/id로만 작성하면 /id 텍스트 경로로 가기때문에
+        변수의값으로 지정 되지않음" 
+
+        */}
+
         {/* path="/movie" 이 경로로 들어오면 detail 페이지로 이동 */}
       </Routes>
     </BrowserRouter>
