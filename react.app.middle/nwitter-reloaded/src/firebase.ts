6524573a53c159d0e51ff8a126a9ccaf442ac72e
sunghyun1014 <sunghyun1014@naver.com>
2023-12-18 23:16:37 +0900

    글작성 기능을 사용하기위해  ,데이터베이스와 storage기능 인증권한을 작성

--- react.app.middle/nwitter-reloaded/src/firebase.ts
+++ react.app.middle/nwitter-reloaded/src/firebase.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // 도메인 , api key 등 여러가지 키 값들이 포함된 config 개체
 /* 이러한 config 옵션을 통해서 아래에 app을 생성하고, 
@@ -19,3 +21,14 @@ const app = initializeApp(firebaseConfig);
 // 로그인 기능을 추가하는 코드
 // 기능을 사용할수 있도록 export 해주기
 export const auth = getAuth(app);
+
+// 2023/12/18 : 데이터베이스와 스토리지에 대한 엑세스 권한 얻기를 해봄
+export const storage = getStorage(app);
+export const db = getFirestore(app);
+
+/* 
+Firebase 의 데이터베이스는 Collectio과 Doucment 라는 것으로 동작함
+Collenction = 폴더
+Document = 데이터값 (예외로 Document 안에 Collenction이 존재)
+
+*/
