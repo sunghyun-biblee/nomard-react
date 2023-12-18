ec60a4d7a4daba572f6afb892c0d88e8bb35172a
sunghyun1014 <sunghyun1014@naver.com>
2023-12-18 23:18:03 +0900

    루트페이지에 새로운 component를 추가

--- react.app.middle/nwitter-reloaded/src/routes/home.tsx
+++ react.app.middle/nwitter-reloaded/src/routes/home.tsx
@@ -1,6 +1,11 @@
 import { useNavigate } from "react-router-dom";
 import { auth } from "../firebase";
+import PostTweetForm from "../components/post-tweet-form";
+import styled from "styled-components";
 
+// 트윗을 올리는 컴포넌트 만들기
+
+const Wrapper = styled.div``;
 export default function Home() {
   const navigate = useNavigate();
   const logout = () => {
@@ -8,8 +13,10 @@ export default function Home() {
     navigate("/login");
   };
   return (
-    <h1>
-      <button onClick={logout}>Log out</button>
-    </h1>
+    <>
+      <Wrapper>
+        <PostTweetForm></PostTweetForm>
+      </Wrapper>
+    </>
   );
 }
