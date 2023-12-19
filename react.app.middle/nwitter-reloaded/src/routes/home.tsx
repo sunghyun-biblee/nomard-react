4a018af91603cfb9f599a27ba076bb32193b5d82
sunghyun1014 <sunghyun1014@naver.com>
2023-12-19 23:09:10 +0900

    home 컴포넌트에 timeline 컴포넌트를 추가하여 게시물을 확인할 수 있도록 만듬

--- react.app.middle/nwitter-reloaded/src/routes/home.tsx
+++ react.app.middle/nwitter-reloaded/src/routes/home.tsx
@@ -2,10 +2,17 @@ import { useNavigate } from "react-router-dom";
 import { auth } from "../firebase";
 import PostTweetForm from "../components/post-tweet-form";
 import styled from "styled-components";
+import Timeline from "../components/timeline";
 
 // 트윗을 올리는 컴포넌트 만들기
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: grid;
+  gap: 50px;
+  overflow-y: scroll;
+  // 게시글 작성 양식은 그대로 고정되어 있는 상태에서 트윗들을 스크롤 할 수 있게
+  grid-template-rows: 1fr 5fr;
+`;
 export default function Home() {
   const navigate = useNavigate();
   const logout = () => {
@@ -16,6 +23,7 @@ export default function Home() {
     <>
       <Wrapper>
         <PostTweetForm></PostTweetForm>
+        <Timeline></Timeline>
       </Wrapper>
     </>
   );
