8b67f7dd61ff2ee8c61532b8002cf6024bffd40a
sunghyun1014 <sunghyun1014@naver.com>
2023-12-22 00:35:46 +0900

    게시글의 내용과 사진을 수정할 수 있게 구현

--- react.app.middle/nwitter-reloaded/src/components/tweet.tsx
+++ react.app.middle/nwitter-reloaded/src/components/tweet.tsx
@@ -1,8 +1,15 @@
 import styled from "styled-components";
 import { ITweet } from "./timeline";
 import { auth, db, storage } from "../firebase";
-import { deleteDoc, doc } from "firebase/firestore";
-import { deleteObject, ref } from "firebase/storage";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
+import {
+  deleteObject,
+  getDownloadURL,
+  ref,
+  uploadBytes,
+} from "firebase/storage";
+import { useState } from "react";
+import { TextArea } from "./post-tweet-form";
 
 const Wrapper = styled.div`
   display: grid;
@@ -12,16 +19,20 @@ const Wrapper = styled.div`
   border-radius: 15px;
   margin-bottom: 10px;
 `;
+
 const Column = styled.div``;
 const Username = styled.span`
+  display: inline-block;
   font-weight: 600;
   font-size: 15px;
+  padding-bottom: 20px;
 `;
 const Payload = styled.p`
   padding: 10px 0 30px 0;
   font-size: 18px;
 `;
 const Photo = styled.img`
+  border-radius: 10px;
   height: 100px;
   width: 100px;
   border: 15px;
@@ -35,10 +46,59 @@ const DeleteButton = styled.button`
   padding: 5px 10px;
   text-transform: uppercase;
   border-radius: 10px;
+  margin: 10px 10px 0 0;
   cursor: pointer;
 `;
+const Imgbox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const EditButton = styled.button`
+  background-color: rgb(83, 71, 255);
+  color: white;
+  font-weight: 600;
+  border: 0;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 10px;
+  margin: 10px 10px 0 0;
+  cursor: pointer;
+`;
+const CancelButton = styled.button`
+  background-color: rgb(1, 0, 10);
+  color: white;
+  font-weight: 600;
+  border: 0;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 10px;
+  margin: 10px 10px 0 0;
+  cursor: pointer;
+`;
+
+const EditPhoto = styled.input`
+  display: none;
+`;
+const EditPhotoLabel = styled.label`
+  display: inline-block;
+  border-radius: 10px;
+  margin-top: 10px;
+  padding: 7px;
+  color: white;
+  /* color: #1d9bf0; */
+  background-color: #1d9bf0;
+  /* background-color: #011f33; */
+`;
+
 export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
   const user = auth.currentUser;
+  const [editMode, setEditMode] = useState(false);
+  const [editTweet, setEditTweet] = useState(tweet);
+  const [file, setFile] = useState<File | null>(null);
 
   // 게시물을 삭제하는 함수
   const onDelete = async () => {
@@ -50,6 +110,9 @@ export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
 
     try {
       await deleteDoc(doc(db, "tweets", id));
+      // doc()은 어떠한값인지 찾기위해 조회함 db : fireDatabase,"tweets" : 콜렉션 , id: 참조값 > frieDatebase에 있는 tweets 폴더안에 해당 id값을 가진 데이터를 가져옴
+      // deleteDoc(doc()) : doc로 조회한 값을 삭제
+
       /*  deleteDoc의 매개변수는 삭제할 문서에 대한 참조이다 (3개의 인자를 필요로함)
       첫번째는 doc함수를 사용하여 참조할 데이터를 불러옴 > firebase의 인스턴스(db)
       두 번째는 문서가 저장된 경로 
@@ -76,20 +139,95 @@ export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
     }
     //원한다면 로딩이나 삭제 중인 것을 상태로 만들어봐라
   };
+  const onTextChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const value = event.target.value;
+    setEditTweet(value);
+  };
+  const onCancel = () => {
+    setEditMode((mode) => !mode);
+  };
+  const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = event.target;
+    console.log(event);
+    console.log(files);
+    if (files && files.length === 1) {
+      if (files[0].size > 1000000) {
+        event.target.value = "";
+        return alert("Photo size too big! \n you can upload under 1MB");
+      }
+      alert("변경 완료");
+      setFile(files[0]);
+    }
+  };
+  const onEdite = async () => {
+    setEditMode((mode) => !mode);
+    if (!editMode) return;
+    try {
+      if (file !== null) {
+        //새 이미지 트윗 업데이트
+        const locationRef = ref(storage, `tweets/${userId}/${id}`);
+        //ref() => 어디에 저장할것인지, 어떤경로에 저장한것인지 작성
+        //storage에 선택한 파일이미지를 업로드함
+
+        const result = await uploadBytes(locationRef, file);
+        //파일을 어디에 저장할 것인지, 그리고 value
+        // 이후 업로드한 이미지의 URL을 받아서 doc에 URL정보를 저장하고싶음
+
+        //uploadBytes(ref > locationRef , data > file)
+        const imgUrl = await getDownloadURL(result.ref);
+        // getDownloadURL(): firebase/sotrage에서 불러옴 , 해당함수는 result의 퍼블릭 URL을 반환함
+        updateDoc(doc(db, "tweets", id), {
+          tweet: editTweet,
+          imgUrl,
+        });
+      } else {
+        //트윗만 업데이트
+        updateDoc(doc(db, "tweets", id), {
+          tweet: editTweet,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setEditMode(false);
+      setFile(null);
+    }
+  };
   return (
     <Wrapper>
       <Column>
         <Username>{username}</Username>
-        <Payload>{tweet}</Payload>
+        {editMode ? (
+          <TextArea onChange={onTextChange} value={editTweet}></TextArea>
+        ) : (
+          <Payload>{tweet}</Payload>
+        )}
         {user?.uid === userId ? (
-          <DeleteButton onClick={onDelete}>Delete</DeleteButton>
+          <>
+            <DeleteButton onClick={onDelete}>Delete</DeleteButton>
+            <EditButton onClick={onEdite}>
+              {editMode ? "save" : "edit"}
+            </EditButton>
+            {editMode ? (
+              <CancelButton onClick={onCancel}>Cancel</CancelButton>
+            ) : null}
+          </>
         ) : null}
       </Column>
-      {photo ? (
-        <Column>
+      {editMode ? (
+        <>
+          <Imgbox>
+            <Photo src={photo}></Photo>
+            <EditPhotoLabel htmlFor="editfile">Edit photo</EditPhotoLabel>
+          </Imgbox>
+        </>
+      ) : photo ? (
+        <Imgbox>
           <Photo src={photo}></Photo>
-        </Column>
+        </Imgbox>
       ) : null}
+
+      <EditPhoto type="file" id="editfile" onChange={onFileChange}></EditPhoto>
     </Wrapper>
   );
 }
